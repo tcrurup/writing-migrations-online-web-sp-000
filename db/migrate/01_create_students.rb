2aebbb1c983bc4d3aef1127dded7fdcdba9ceb1a
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 04:29:49 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
     def change
-      create_table :students
+      create_table :students do |t|
+        t.string :name
+      end
     end
 end
